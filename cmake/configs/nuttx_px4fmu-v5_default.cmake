424fbebe54e91d44b6bb4b599a13067febc74682
mark_an <shuai_an@outlook.com>
2018-08-01 14:55:51 +0800

    Modified: update nuttx_px4fmu-v5_default.cmake

--- cmake/configs/nuttx_px4fmu-v5_default.cmake
+++ cmake/configs/nuttx_px4fmu-v5_default.cmake
@@ -1,7 +1,7 @@
 
-px4_nuttx_configure(HWCLASS m7 CONFIG nsh ROMFS y ROMFSROOT px4fmu_common IO px4io-v2)
+#px4_nuttx_configure(HWCLASS m7 CONFIG nsh ROMFS y ROMFSROOT px4fmu_common IO px4io-v2)
 
-#px4_nuttx_configure(HWCLASS m7 CONFIG nsh ROMFS y ROMFSROOT px4fmu_common)
+px4_nuttx_configure(HWCLASS m7 CONFIG nsh ROMFS y ROMFSROOT px4fmu_common)
 
 set(config_uavcan_num_ifaces 2)
 
@@ -29,9 +29,9 @@ set(config_module_list
 	drivers/oreoled
 	drivers/pwm_input
 	drivers/pwm_out_sim
-	drivers/px4flow
+	#drivers/px4flow
 	drivers/px4fmu
-	drivers/px4io
+	#drivers/px4io
 	drivers/rgbled
 	drivers/rgbled_pwm
 	drivers/stm32
