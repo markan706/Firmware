976416c3cc3d383f76356365704492f10c643dba
mark_an <shuai_an@outlook.com>
2018-08-02 18:48:35 +0800

    Modified: update ma40h1s_JZL.cpp

--- src/drivers/ma40h1s/ma40h1s_JZL.cpp
+++ src/drivers/ma40h1s/ma40h1s_JZL.cpp
@@ -1145,8 +1145,8 @@ int MA40H1S::timer5_interrupt(int irq, void *context, void *arg)
                 rCR2 |= ADC_CR2_ADON;
                 rCR2 |= ADC_CR2_SWSTART;
                 _start_time = hrt_absolute_time();
-				//PX4_INFO("timer5_interrupt running");
-                trig_state = 5;
+				PX4_INFO("timer5_interrupt running");
+                trig_state = 5;s
 
                 break;
 		default:
