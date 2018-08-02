220405444d55265c9e9d3e80a738a8e796bbda99
mark_an <shuai_an@outlook.com>
2018-08-02 13:56:08 +0800

    Modified: update ma40h1s_JZL.cpp

--- src/drivers/ma40h1s/ma40h1s_JZL.cpp
+++ src/drivers/ma40h1s/ma40h1s_JZL.cpp
@@ -491,10 +491,10 @@ int MA40H1S::init()
     //rSMPR2 = 0b00 000 000 000 000 010 000 000 000 000 000; //  0--9  Channel 5
     rSMPR1 = 0b00000000000000000100000000000000; //  set sample time of adc_ch14 to 010 (28T)
     rSMPR2 = 0b00000000000000000100000000000000; // set sample time of adc_ch4 to 010 (28T)
-    PX4_INFO("ADC setting");
-    return ret;
     rCR1 = ADC_CR1_RES_12BIT; //Resolution
     rCR2 = 0;
+    PX4_INFO("ADC setting");
+    return ret;
     rSQR1 = 0;
     rSQR2 = 0;
     //rSQR3 = 15;  /* will be updated with the channel each tick */
