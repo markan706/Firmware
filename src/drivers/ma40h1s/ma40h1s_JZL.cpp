274bde49802e9a3319821253e67e3da3522f9939
mark_an <shuai_an@outlook.com>
2018-08-03 20:40:46 +0800

    Modified: update ma40h1s_JZL.cpp -- using 1 ultrasonic dev and ADC2 with DMA

--- src/drivers/ma40h1s/ma40h1s_JZL.cpp
+++ src/drivers/ma40h1s/ma40h1s_JZL.cpp
@@ -403,6 +403,7 @@ int MA40H1S::init()
     io_timer_channel_init(_ultrasonic_config[0].pwm2_ch, IOTimerChanMode_PWMOut, NULL, NULL); // init PWM CH7/CH5
     io_timer_channel_init(_ultrasonic_config[0].pwm1_ch, IOTimerChanMode_PWMOut, NULL, NULL); // init PWM CH8/CH6
     io_timer_set_rate(_ultrasonic_config[0].timer_index, 40000); //timer_index 1: TIM4   timer_index 2: TIM12
+    io_timer_set_enable(true, IOTimerChanMode_PWMOut, 0b00110000);
     // io_timer_set_ccr(_ultrasonic_config[0].pwm2_ch, 12);
     // io_timer_set_ccr(_ultrasonic_config[0].pwm1_ch, 12);
     // io_timer_trigger();
