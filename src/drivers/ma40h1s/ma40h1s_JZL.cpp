5ba67e9ee672bb523c3f73cbf01c1afd0ebe95ff
mark_an <shuai_an@outlook.com>
2018-08-04 19:04:43 +0800

    Modified: update drv_io_timer.c

--- src/drivers/ma40h1s/ma40h1s_JZL.cpp
+++ src/drivers/ma40h1s/ma40h1s_JZL.cpp
@@ -426,7 +426,7 @@ int MA40H1S::init()
 
     // io_timer_channel_init(_ultrasonic_config[0].pwm2_ch, IOTimerChanMode_PWMOut, NULL, NULL); // init PWM CH7/CH5
     // io_timer_channel_init(_ultrasonic_config[0].pwm1_ch, IOTimerChanMode_PWMOut, NULL, NULL); // init PWM CH8/CH6
-    io_timer_set_rate(_ultrasonic_config[0].timer_index, 40000); //timer_index 1: TIM4   timer_index 2: TIM12
+    io_timer_set_rate(_ultrasonic_config[0].timer_index+2, 40000); //timer_index 1: TIM4   timer_index 2: TIM12
     io_timer_set_enable(true, IOTimerChanMode_PWMOut, 0b00110000);
     // io_timer_set_ccr(_ultrasonic_config[0].pwm2_ch, 12);
     // io_timer_set_ccr(_ultrasonic_config[0].pwm1_ch, 12);
@@ -983,7 +983,7 @@ out:
 				channel_mask &= ~(1 << channel);
 			}
 		}
-	    io_timer_set_rate(_ultrasonic_config[0].timer_index, 40000); //timer_index 1: TIM4   timer_index 2: TIM12
+	    io_timer_set_rate(_ultrasonic_config[0].timer_index+2, 40000); //timer_index 1: TIM4   timer_index 2: TIM12
 	    io_timer_set_enable(true, IOTimerChanMode_PWMOut, 0b00110000);
 
         rSQR3 = _ultrasonic_config[k].adc_ch;
@@ -1136,7 +1136,7 @@ int MA40H1S::timer5_interrupt(int irq, void *context, void *arg)
                if ((*pdev_id) == MA40H1S_ID_PRIMARY) {
                     // io_timer_set_enable(true, IOTimerChanMode_PWMOut, 0b11111110); // PWM-ch5 PWM-ch6
                     //io_timer_set_ccr(_ultrasonic_config[0].pwm2_ch - 1, 12);
-					io_timer_set_ccr(_ultrasonic_config[0].pwm2_ch - 1, 12);
+					io_timer_set_ccr(_ultrasonic_config[0].pwm2_ch-1, 12);
 					io_timer_trigger();
                     // printf("enable timer, device id = %d\n", (int)(*pdev_id));
                }
