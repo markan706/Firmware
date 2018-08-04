3a4a32aaa54d911ff82c2e40835ddc6c809784a8
mark_an <shuai_an@outlook.com>
2018-08-04 11:36:53 +0800

    Modified: update ma40h1s_JZL.cpp

--- src/drivers/stm32/drv_io_timer.c
+++ src/drivers/stm32/drv_io_timer.c
@@ -801,8 +801,7 @@ int io_timer_set_enable(bool state, io_timer_channel_mode_t mode, io_timer_chann
 		/* Yes - we provide them */
 
 		masks = channel_allocations[mode];
-		//printf("io_timer_set_enable() masks = %u\n", masks);
-		PX4_INFO("io_timer_set_enable() masks = %u\n", masks);
+		printf("io_timer_set_enable() masks = %u\n", masks);
 
 	} else {
 
@@ -811,8 +810,7 @@ int io_timer_set_enable(bool state, io_timer_channel_mode_t mode, io_timer_chann
 		/* Only allow the channels in that mode to be affected */
 
 		masks &= channel_allocations[mode];
-		//printf("io_timer_set_enable() else masks = %u\n", masks);
-		PX4_INFO("io_timer_set_enable() else masks = %u\n", masks);
+		printf("io_timer_set_enable() else masks = %u\n", masks);
 
 	}
 
